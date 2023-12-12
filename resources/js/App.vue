3a31f2c0ddee150af68e6984fea83b237226ce66
pousvs <savanhsith@gmail.com>
2023-12-12 23:29:12 +0700

    test v1

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>App v2</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WorkspaceJsonApp',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
